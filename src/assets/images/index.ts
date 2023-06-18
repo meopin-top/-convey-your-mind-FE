18de34217901e71d96e728d8e83b3eeb3dbe6887
mochang2 <qjsrodksro@gmail.com>
2023-06-18 21:36:52 +0900

    feat: 로그인 페이지에 필요한 asset(이미지) 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,6 @@
+import emailLogo from "./email-logo.webp"
 import home from "./home.webp"
+import kakaoLogo from "./kakao-logo.webp"
 import pageNotFound from "./page-not-found.webp"
 
-export {home, pageNotFound}
+export {emailLogo, home, kakaoLogo, pageNotFound}
