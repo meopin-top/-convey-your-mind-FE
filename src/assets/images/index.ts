132670c53d4321274165be797bf0000f4a438e6c
changmolee <changmolee@netmable.com>
2023-07-05 17:39:44 +0900

    feat: 네이버 로고 추가(index.ts, naver-logo.webp)

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,6 +1,7 @@
 import emailLogo from "./email-logo.webp"
 import home from "./home.webp"
 import kakaoLogo from "./kakao-logo.webp"
+import naverLogo from "./naver-logo.webp"
 import pageNotFound from "./page-not-found.webp"
 
-export {emailLogo, home, kakaoLogo, pageNotFound}
+export {emailLogo, home, kakaoLogo, naverLogo, pageNotFound}
