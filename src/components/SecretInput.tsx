5682b41b5fcf4d8dbe5214421354bbface3c719c
mochang2 <qjsrodksro@gmail.com>
2023-07-08 19:59:07 +0900

    feat: SecretInput 컴포넌트 생성
    - 컴포넌트(SecretInput.tsx)
    - barrel(index.ts)
    - 스타일(index.scss, secret-input.scss)

---
+++ src/components/SecretInput.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import {useState, type InputHTMLAttributes, type MutableRefObject} from "react"
+import {EyeClose, EyeOpen} from "@/assets/icons"
+import type {TIconSize} from "@/@types/style"
+
+type TProps = Omit<InputHTMLAttributes<HTMLInputElement>, "type" | "size"> & {
+  inputRef?: MutableRefObject<HTMLInputElement | null>
+  size?: TIconSize
+}
+
+const SecretInput = ({className, inputRef, size = "md", ...props}: TProps) => {
+  const [isShowing, setIsShowing] = useState(false)
+
+  function handleIsShowing() {
+    setIsShowing(!isShowing)
+  }
+
+  return (
+    <div className={`secret-input ${className}`}>
+      <input
+        type={`${isShowing ? "text" : "password"}`}
+        ref={inputRef}
+        {...props}
+      />
+      <div className="icon-placer f-center mr-1" onClick={handleIsShowing}>
+        {isShowing ? (
+          <EyeOpen className={size} />
+        ) : (
+          <EyeClose className={size} />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default SecretInput
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import Loading from "./Loading"
+import PortalLoading from "./PortalLoading"
+import SecretInput from "./SecretInput"
+
+export {Loading, PortalLoading, SecretInput}
