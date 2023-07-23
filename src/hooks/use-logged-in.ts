eb94eb8c2211257c2dc3c58e8c4f98ec52335189
mochang2 <qjsrodksro@gmail.com>
2023-07-23 10:41:51 +0900

    fix: redirect에서 useRouter를 이용하도록 변경(use-logged-in.ts)
    - next/navigation의 redirect를 이용하면 SSR이 진행되기 전에 빈 화면이
      나타남
    - alert를 띄워야하기 때문에 redirect는 프론트 측에서 진행해야 함
    - useRouter를 이용하면 redirect 시 빈 화면이 나타나지 않고, loading을
      띄워줄 수 있음

--- src/hooks/use-logged-in.ts
+++ src/hooks/use-logged-in.ts
@@ -1,26 +1,40 @@
-import {useLayoutEffect} from "react"
-import {redirect} from "next/navigation"
+import {useState, useLayoutEffect} from "react"
+import {useRouter} from "next/navigation"
 import Storage from "@/store/local-storage"
 import ROUTE from "@/constants/route"
 
-export function useNeedLoggedIn() {
+export function useNeedLoggedIn(): boolean {
+  const [isPainted, setIsPainted] = useState(false)
+  const router = useRouter()
+
   useLayoutEffect(() => {
     const isNotLoggedIn = !new Storage().get("nickName")
 
     if (isNotLoggedIn) {
       alert("로그인 후에 서비스 이용해주세요.")
 
-      redirect(ROUTE.MAIN)
+      router.push(ROUTE.MAIN)
     }
-  }, [])
+
+    setIsPainted(true)
+  }, [router])
+
+  return isPainted
 }
 
-export function useNeedNotLoggedIn() {
+export function useNeedNotLoggedIn(): boolean {
+  const [isPainted, setIsPainted] = useState(false)
+  const router = useRouter()
+
   useLayoutEffect(() => {
     const isLoggedIn = new Storage().get("nickName")
 
     if (isLoggedIn) {
-      redirect(ROUTE.MY_PAGE)
+      router.push(ROUTE.MAIN)
     }
-  }, [])
+
+    setIsPainted(true)
+  }, [router])
+
+  return isPainted
 }
