88c5c46c6fe293a1c9bd1991e2dfb4649311750f
mochang2 <qjsrodksro@gmail.com>
2023-07-23 10:50:36 +0900

    refactor: 훅 공통 로직 함수로 분리(use-logged-in.ts)

--- src/hooks/use-logged-in.ts
+++ src/hooks/use-logged-in.ts
@@ -3,38 +3,30 @@ import {useRouter} from "next/navigation"
 import Storage from "@/store/local-storage"
 import ROUTE from "@/constants/route"
 
-export function useNeedLoggedIn(): boolean {
-  const [isPainted, setIsPainted] = useState(false)
-  const router = useRouter()
-
-  useLayoutEffect(() => {
-    const isNotLoggedIn = !new Storage().get("nickName")
-
-    if (isNotLoggedIn) {
-      alert("로그인 후에 서비스 이용해주세요.")
-
-      router.push(ROUTE.MAIN)
-    }
-
-    setIsPainted(true)
-  }, [router])
-
-  return isPainted
-}
-
-export function useNeedNotLoggedIn(): boolean {
+function useCheckLoginStatus(shouldBeLoggedIn: boolean): boolean {
   const [isPainted, setIsPainted] = useState(false)
   const router = useRouter()
 
   useLayoutEffect(() => {
     const isLoggedIn = new Storage().get("nickName")
 
-    if (isLoggedIn) {
+    if (shouldBeLoggedIn && !isLoggedIn) {
+      alert("로그인 후에 서비스를 이용해주세요.")
+      router.push(ROUTE.MAIN)
+    } else if (!shouldBeLoggedIn && isLoggedIn) {
       router.push(ROUTE.MY_PAGE)
     }
 
     setIsPainted(true)
-  }, [router])
+  }, [router, shouldBeLoggedIn])
 
   return isPainted
+}
+
+export function useNeedLoggedIn(): boolean {
+  return useCheckLoginStatus(true)
+}
+
+export function useNeedNotLoggedIn(): boolean {
+  return useCheckLoginStatus(false)
 }
