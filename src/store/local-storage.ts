9bbfb53b163bc23e54e18a4d148887cd94227958
mochang2 <qjsrodksro@gmail.com>
2023-08-12 19:27:50 +0900

    fix: local storage window 객체가 존재할 때만 접근하도록 수정(local-storage.ts)

--- src/store/local-storage.ts
+++ src/store/local-storage.ts
@@ -2,15 +2,21 @@ import type {TLocalStorageKey} from "@/@types/storage"
 
 export default class Storage {
   static get(key: TLocalStorageKey) {
-    return localStorage.getItem(key)
+    if (typeof window !== "undefined") {
+      return localStorage.getItem(key)
+    }
+
+    return null
   }
 
   static set(key: TLocalStorageKey, value: string) {
-    localStorage.setItem(key, value)
+    if (typeof window !== "undefined") {
+      localStorage.setItem(key, value)
+    }
   }
 
   static remove(key: TLocalStorageKey): boolean {
-    if (localStorage.getItem(key)) {
+    if (typeof window !== "undefined" && localStorage.getItem(key)) {
       localStorage.removeItem(key)
 
       return true
