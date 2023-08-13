179296bd26b4c55a40050ab7589c98a6dceb6a2d
mochang2 <qjsrodksro@gmail.com>
2023-08-13 21:13:19 +0900

    test: body scroll lock 관련 테스트 추가(BottomSheet.test.tsx)

--- src/__tests__/components/BottomSheet.test.tsx
+++ src/__tests__/components/BottomSheet.test.tsx
@@ -2,11 +2,22 @@ import type {HTMLAttributes} from "react"
 import {render, fireEvent, screen} from "@testing-library/react"
 import BottomSheet from "@/components/BottomSheet"
 
+const lockScrollMock = jest.fn()
+const unlockScrollMock = jest.fn()
+
 jest.mock("../../assets/icons/close.svg", () => ({
   __esModule: true,
   default: ({...rest}: HTMLAttributes<HTMLOrSVGElement>) => (
     <svg {...rest}>close</svg>
   ),
+}))
+
+jest.mock("../../hooks/use-body-scroll-lock.ts", () => ({
+  __esModule: true,
+  default: () => ({
+    lockScroll: lockScrollMock,
+    unlockScroll: unlockScrollMock,
+  }),
 }))
 
 describe("BottomSheet", () => {
@@ -15,14 +26,14 @@ describe("BottomSheet", () => {
     window.removeEventListener = jest.fn()
   })
 
-  afterAll(() => {
+  afterEach(() => {
     jest.clearAllMocks()
   })
 
   it("isOpen props가 true면 background는 open 클래스명을 가져야 한다.", () => {
     // given, when
     render(
-      <BottomSheet isOpen={true} onClose={jest.fn()}>
+      <BottomSheet isOpen onClose={jest.fn()}>
         Content
       </BottomSheet>
     )
@@ -50,7 +61,7 @@ describe("BottomSheet", () => {
   it("isHandlingHistory props가 true면 이벤트 리스너(popstate) API가 호출되어야 한다.", () => {
     // given, when
     const {unmount} = render(
-      <BottomSheet isOpen={true} onClose={jest.fn()} isHandlingHistory={true}>
+      <BottomSheet isOpen onClose={jest.fn()} isHandlingHistory>
         Content
       </BottomSheet>
     )
@@ -76,7 +87,7 @@ describe("BottomSheet", () => {
     const onCloseMock = jest.fn()
 
     render(
-      <BottomSheet isOpen={true} onClose={onCloseMock}>
+      <BottomSheet isOpen onClose={onCloseMock}>
         Content
       </BottomSheet>
     )
@@ -94,7 +105,7 @@ describe("BottomSheet", () => {
     const onCloseMock = jest.fn()
 
     render(
-      <BottomSheet isOpen={true} onClose={onCloseMock}>
+      <BottomSheet isOpen onClose={onCloseMock}>
         Content
       </BottomSheet>
     )
@@ -108,10 +119,10 @@ describe("BottomSheet", () => {
     expect(onCloseMock).not.toBeCalled()
   })
 
-  it("isShowingClose가 false면 닫기 버튼이 렌더링되지 말아야 한다.", () => {
+  it("isShowingClose props가 false면 닫기 버튼이 렌더링되지 말아야 한다.", () => {
     // given, when
     render(
-      <BottomSheet isOpen={true} isShowingClose={false} onClose={jest.fn()}>
+      <BottomSheet isOpen isShowingClose={false} onClose={jest.fn()}>
         content
       </BottomSheet>
     )
@@ -122,10 +133,10 @@ describe("BottomSheet", () => {
     expect(closeButton).not.toBeInTheDocument()
   })
 
-  it("isShowingClose가 true면 닫기 버튼이 렌더링되어야 한다.", () => {
+  it("isShowingClose props가 true면 닫기 버튼이 렌더링되어야 한다.", () => {
     // given, when
     render(
-      <BottomSheet isOpen={true} isShowingClose={true} onClose={jest.fn()}>
+      <BottomSheet isOpen isShowingClose onClose={jest.fn()}>
         content
       </BottomSheet>
     )
@@ -136,12 +147,12 @@ describe("BottomSheet", () => {
     expect(closeButton).toBeInTheDocument()
   })
 
-  it("isShowingClose가 true일 때 닫기 버튼을 누르면 onClose가 호출되어야 한다.", () => {
+  it("isShowingClose props가 true일 때 닫기 버튼을 누르면 onClose가 호출되어야 한다.", () => {
     // given
     const onCloseMock = jest.fn()
 
     render(
-      <BottomSheet isOpen={true} isShowingClose={true} onClose={onCloseMock}>
+      <BottomSheet isOpen isShowingClose onClose={onCloseMock}>
         content
       </BottomSheet>
     )
@@ -153,5 +164,41 @@ describe("BottomSheet", () => {
 
     // then
     expect(onCloseMock).toHaveBeenCalledTimes(1)
+  })
+
+  it("isControllingScroll props이 false일 때 lockScroll도, openScroll도 호출되지 않는다.", () => {
+    // given, when
+    render(
+      <BottomSheet isOpen isControllingScroll={false} onClose={jest.fn()}>
+        content
+      </BottomSheet>
+    )
+
+    expect(lockScrollMock).not.toBeCalled()
+    expect(unlockScrollMock).not.toBeCalled()
+  })
+
+  it("isControllingScroll props이 true이고 isOpen props가 true일 때 lockScroll이 호출된다.", () => {
+    // given, when
+    render(
+      <BottomSheet isOpen isControllingScroll onClose={jest.fn()}>
+        content
+      </BottomSheet>
+    )
+
+    expect(lockScrollMock).toBeCalled()
+    expect(unlockScrollMock).not.toBeCalled()
+  })
+
+  it("isControllingScroll props이 true이고 isOpen props가 true일 때 unlockScroll이 호출된다.", () => {
+    // given, when
+    render(
+      <BottomSheet isOpen={false} isControllingScroll onClose={jest.fn()}>
+        content
+      </BottomSheet>
+    )
+
+    expect(lockScrollMock).not.toBeCalled()
+    expect(unlockScrollMock).toBeCalled()
   })
 })
