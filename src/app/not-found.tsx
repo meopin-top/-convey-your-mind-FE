53d951055858020384dc37134ec5f6076a89e98b
mochang2 <qjsrodksro@gmail.com>
2023-08-19 12:07:49 +0900

    fix: not-found 페이지 무한 리로드 버그 수정
    - 페이지 선언(page.tsx)
    - alert 제거(not-found.tsx)
    - 스타일 선언(index.scss, not-found.scss)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,6 @@
-"use client"
-
-import {useEffect} from "react"
-import Image from "next/image"
-import {pageNotFound} from "@/assets/images"
-
 const NotFound = () => {
-  useEffect(() => {
-    alert("존재하지 않는 페이지입니다.")
-  }, [])
-
   return (
-    <Image src={pageNotFound} alt="존재하지 않는 페이지입니다." width={720} />
+    <div className="root-wrapper not-found">존재하지 않는 페이지입니다.</div>
   )
 }
 
