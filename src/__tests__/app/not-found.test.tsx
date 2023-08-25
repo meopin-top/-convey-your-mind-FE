082b4e5faa47b16f3a0387c1e277718eff0c7e7e
mochang2 <qjsrodksro@gmail.com>
2023-08-26 08:41:00 +0900

    test: alert 테스트 제거, 렌더링 테스트 변경(not-found.test.tsx)

--- src/__tests__/app/not-found.test.tsx
+++ src/__tests__/app/not-found.test.tsx
@@ -1,25 +1,14 @@
 import {render, screen} from "@testing-library/react"
 import NotFound from "@/app/not-found"
-import {createAlertMock} from "@/__mocks__/window"
 
 describe("not-found", () => {
-  beforeEach(() => {
-    createAlertMock()
-  })
-
-  it("이미지가 올바르게 렌더링된다.", () => {
+  it("wrapper가 올바르게 렌더링된다.", () => {
     // given, when
     render(<NotFound />)
 
-    const image = screen.getByAltText("존재하지 않는 페이지입니다.")
+    const wrapper = screen.getByText("존재하지 않는 페이지입니다.")
 
     // then
-    expect(image).toBeInTheDocument()
-  })
-
-  it("window.alert가 호출된다.", () => {
-    render(<NotFound />)
-
-    expect(window.alert).toHaveBeenCalledTimes(1)
+    expect(wrapper).toBeInTheDocument()
   })
 })
