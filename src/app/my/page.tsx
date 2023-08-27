3ee5ebf64f0085029a0efad6dfab988fcfd8d031
mochang2 <qjsrodksro@gmail.com>
2023-08-27 18:53:31 +0900

    refactor: my 페이지에서 UserInformation 컴포넌트 분리(page.tsx, user.scss, my.scss)

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -1,4 +1,5 @@
-import {NeedLoggedIn} from "@/components"
+import Link from "next/link"
+import {NeedLoggedIn, Header} from "@/components"
 import {
   UserInformation,
   RollingPaperParticipation,
@@ -12,8 +13,16 @@ const MyPage = () => {
       <NeedLoggedIn />
 
       <div className="my-page root-wrapper">
+        <Header />
+
         <main className="main f-center">
-          <UserInformation />
+          <UserInformation
+            right={
+              <button className="profile-edit xxxs radius-sm mt-2">
+                <Link href={"#"}>프로필 편집</Link>
+              </button>
+            }
+          />
           <RollingPaperParticipation />
           <ProjectsInProgressing />
           <ReceivedRollingPapers />
--- src/components/my/UserInformation.tsx
+++ src/components/my/UserInformation.tsx
@@ -1,11 +1,14 @@
 "use client"
 
-import {useState, useLayoutEffect} from "react"
+import {useState, useLayoutEffect, type ReactNode} from "react"
 import Image from "next/image"
-import Link from "next/link"
 import Storage from "@/store/local-storage"
 
-const UserInformation = () => {
+type TProps = {
+  right: ReactNode
+}
+
+const UserInformation = ({right}: TProps) => {
   const [profile, setProfile] = useState("")
   const [nickName, setNickName] = useState("")
 
@@ -22,9 +25,7 @@ const UserInformation = () => {
       <div className="nick-name">
         <h1>반가워요!</h1>
         <h1 suppressHydrationWarning>{nickName}님!</h1>
-        <button className="profile-edit xxxs radius-sm mt-2">
-          <Link href={"#"}>프로필 편집</Link>
-        </button>
+        {right}
       </div>
     </div>
   )
