d9af7311038f31c0e335471b5047b0190146ea49
mochang2 <qjsrodksro@gmail.com>
2023-08-27 18:55:48 +0900

    test: 헤더 컴포넌트 모킹(my.test.tsx)

--- src/__tests__/app/my.test.tsx
+++ src/__tests__/app/my.test.tsx
@@ -2,6 +2,7 @@ import {render, screen} from "@testing-library/react"
 import MyPage from "@/app/my/page"
 
 const NEED_LOGGED_IN = "로그인 필요"
+const HEADER = "헤더"
 const USER_INFORMATION = "유저 정보"
 const ROLLING_PAPER_PARTICIPATION = "롤링페이퍼 참여하기"
 const PROJECTS_IN_PROGRESSING = "참여 중인 프로젝트"
@@ -10,6 +11,11 @@ const RECEIVED_ROLLING_PAPERS = "내가 받은 롤링페이퍼"
 jest.mock("../../components/LoginChecker", () => ({
   __esModule: true,
   NeedLoggedIn: () => <div>{NEED_LOGGED_IN}</div>,
+}))
+
+jest.mock("../../components/Header", () => ({
+  __esModule: true,
+  default: () => <div>{HEADER}</div>,
 }))
 
 jest.mock("../../components/my/UserInformation", () => ({
@@ -38,6 +44,7 @@ describe("MyPage", () => {
     render(<MyPage />)
 
     const needLoggedIn = screen.getByText(NEED_LOGGED_IN)
+    const header = screen.getByText(HEADER)
     const userInformation = screen.getByText(USER_INFORMATION)
     const rollingPaperParticipation = screen.getByText(
       ROLLING_PAPER_PARTICIPATION
@@ -47,6 +54,7 @@ describe("MyPage", () => {
 
     // then
     expect(needLoggedIn).toBeInTheDocument()
+    expect(header).toBeInTheDocument()
     expect(userInformation).toBeInTheDocument()
     expect(rollingPaperParticipation).toBeInTheDocument()
     expect(projectsInProgressing).toBeInTheDocument()
