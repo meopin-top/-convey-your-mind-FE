8b41de3de4320f0f1ad071ff0e53fa57d5f9050f
mochang2 <qjsrodksro@gmail.com>
2023-08-27 18:01:11 +0900

    feat: 헤더에 사용되는 asset 추가(hamburger.svg, logo.webp, index.ts)

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,8 +1,9 @@
 import Close from "./close.svg"
 import EyeClose from "./eye-close.svg"
 import EyeOpen from "./eye-open.svg"
+import Hamburger from "./hamburger.svg"
 import List from "./list.svg"
 import Plus from "./plus.svg"
 import Spinner from "./spinner.svg"
 
-export {Close, EyeClose, EyeOpen, List, Plus, Spinner}
+export {Close, EyeClose, EyeOpen, Hamburger, List, Plus, Spinner}
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,6 +1,7 @@
 import emailLogo from "./email-logo.webp"
 import home from "./home.webp"
 import kakaoLogo from "./kakao-logo.webp"
+import logo from "./logo.webp"
 import naverLogo from "./naver-logo.webp"
 
-export {emailLogo, home, kakaoLogo, naverLogo}
+export {emailLogo, home, kakaoLogo, logo, naverLogo}
