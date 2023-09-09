d941f85c7ba1ee3eed825c1cdfbb982e78d89703
mochang2 <qjsrodksro@gmail.com>
2023-09-09 16:32:15 +0900

    test: 렌더링 테스트 구현(Header.test.tsx)

---
+++ src/__tests__/components/Header.test.tsx
@@ -0,0 +1,33 @@
+import {render, screen, waitFor} from "@testing-library/react"
+import Header from "@/components/Header"
+
+jest.mock("../../components/NavigationBar", () => ({
+  __esModule: true,
+  default: () => <div>네비게이션 바</div>,
+}))
+
+describe("Header", () => {
+  it("로고를 렌더링한다.", async () => {
+    // given, when
+    render(<Header />)
+
+    const image = screen.getByAltText("logo")
+
+    // then
+    await waitFor(() => {
+      expect(image).toBeInTheDocument()
+    })
+  })
+
+  it("네이게이션 바를 렌더링한다.", async () => {
+    // given, when
+    render(<Header />)
+
+    const navigationBar = screen.getByText("네비게이션 바")
+
+    // then
+    await waitFor(() => {
+      expect(navigationBar).toBeInTheDocument()
+    })
+  })
+})
