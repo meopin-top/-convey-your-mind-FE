823b7a7d4e60be75635707aa6ae9d3d6c8f3c3e6
mochang2 <qjsrodksro@gmail.com>
2023-09-09 12:48:32 +0900

    remove: 헤더 슬로건 링크 제거(Header.tsx)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image"
-import Link from "next/link"
 import dynamic from "next/dynamic"
 import {logo} from "@/assets/images"
-import ROUTE from "@/constants/route"
 
 const NavigationBar = dynamic(() => import("./NavigationBar"), {
   loading: () => <></>,
@@ -11,10 +9,7 @@ const NavigationBar = dynamic(() => import("./NavigationBar"), {
 const Header = () => {
   return (
     <header className="mb-4">
-      {/* 현재로서는 Header를 로그인한 이후의 페이지에서만 사용하기 때문에 MY_PAGE로 이동 */}
-      <Link href={ROUTE.MY_PAGE}>
-        <Image src={logo} alt="logo" height={32} />
-      </Link>
+      <Image src={logo} alt="logo" height={32} />
       <NavigationBar />
     </header>
   )
