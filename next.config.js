350cbb8ac226ddd67c55ee5159c6355a65491a7a
mochang2 <qjsrodksro@gmail.com>
2023-09-23 18:34:20 +0900

    feat: next 설정에 이미지 cdn 추가(next.config.js)

--- next.config.js
+++ next.config.js
@@ -7,6 +7,28 @@ const nextConfig = {
     })
     return config
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "k.kakaocdn.net", // 카카오
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ssl.pstatic.net", // 네이버
+        port: "",
+        pathname: "/static/**",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com", // GCP
+        port: "",
+        pathname: "/convey-your-mind-dev-bucket/**",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
