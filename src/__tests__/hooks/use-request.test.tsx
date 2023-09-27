9de61e48599f5fe40a75f065742949267467f58d
mochang2 <qjsrodksro@gmail.com>
2023-09-27 21:46:50 +0900

    test: Date 모킹, AbortController 테스트 추가(window.ts, use-request.test.ts)

--- src/__tests__/hooks/use-request.test.tsx
+++ src/__tests__/hooks/use-request.test.tsx
@@ -5,10 +5,19 @@ import useLogOut from "@/hooks/use-log-out"
 import {
   createFetchMock,
   createAlertMock,
+  createDateMock,
   deleteFetchMock,
+  deleteDateMock,
 } from "@/__mocks__/window"
 import {UNAUTHORIZED} from "@/constants/response-code"
 
+const abortMock = jest.fn()
+
+// @ts-ignore
+global.AbortController = jest.fn(() => ({
+  abort: abortMock,
+  signal: null,
+}))
 jest.mock("../../hooks/use-log-out.ts")
 
 const testid = "error-box"
@@ -39,10 +48,13 @@ describe("useRequest", () => {
 
   afterEach(() => {
     deleteFetchMock()
+    deleteDateMock()
+    jest.clearAllMocks()
   })
 
   it("처음에는 초깃값을 반환한다.", () => {
     // given, when
+    createDateMock({})
     createFetchMock(jest.fn().mockResolvedValueOnce(undefined))
 
     const {result} = renderHook(() => useRequest())
@@ -54,7 +66,10 @@ describe("useRequest", () => {
 
   it("request 호출 시 에러가 없다면 isLoading true를 반환한 뒤 request 끝난 이후 isLoading false를 반환한다.", async () => {
     // given, when
-    createFetchMock(jest.fn().mockResolvedValueOnce(undefined))
+    createDateMock({})
+    createFetchMock(
+      jest.fn().mockResolvedValueOnce({ok: true, json: () => Promise.resolve()})
+    )
 
     render(<TestComponent />)
 
@@ -78,6 +93,7 @@ describe("useRequest", () => {
 
   it("request 호출 시 에러가 있다면 isLoading true를 반환한 뒤 error가 설정되고, 다시 isLoading false을 반환한다.", async () => {
     // given, when
+    createDateMock({})
     createFetchMock(jest.fn().mockResolvedValueOnce(new Error("test")))
 
     render(<TestComponent />)
@@ -107,6 +123,7 @@ describe("useRequest", () => {
 
   it("request 호출 시 응답 코드가 UNAUTHORIZED라면 alert 호출 후 로그아웃 처리한다.", async () => {
     // given, when
+    createDateMock({})
     const logOutMock = jest.fn()
     ;(useLogOut as jest.Mock).mockReturnValue(logOutMock)
     createFetchMock(
@@ -134,4 +151,60 @@ describe("useRequest", () => {
       expect(logOutMock).toBeCalled()
     })
   })
+
+  it("500ms 이내에 같은 API 경로로 요청이 발생하면 이전 API 호출을 중단한다.", async () => {
+    // given
+    createFetchMock(
+      jest.fn().mockResolvedValue({ok: true, json: () => Promise.resolve()})
+    )
+    createDateMock({dateTime: 100, delta: 200})
+
+    render(<TestComponent />)
+
+    const button = screen.getByRole("button") as HTMLButtonElement
+
+    // when
+    fireEvent.click(button)
+    await waitFor(() => {
+      expect(button).toBeDisabled()
+    })
+    await waitFor(() => {
+      expect(button).not.toBeDisabled()
+    })
+    fireEvent.click(button)
+
+    // then
+    expect(abortMock).toBeCalledTimes(1)
+  })
+
+  it("500ms를 넘은 간격으로 같은 API 경로로 요청이 발생하면 이전 API 호출을 중단하지 않는다.", async () => {
+    // given
+    createFetchMock(
+      jest.fn().mockResolvedValue({ok: true, json: () => Promise.resolve()})
+    )
+    createDateMock({dateTime: 100, delta: 1000})
+
+    render(<TestComponent />)
+
+    const button = screen.getByRole("button") as HTMLButtonElement
+
+    // when
+    fireEvent.click(button)
+    await waitFor(() => {
+      expect(button).toBeDisabled()
+    })
+    await waitFor(() => {
+      expect(button).not.toBeDisabled()
+    })
+    fireEvent.click(button)
+
+    // then
+    expect(abortMock).not.toBeCalled()
+  })
+
+  // it("500번대 에러가 반환되면 '서버 측 오류'가 콘솔에 출력된다.", () => {})
+
+  // it("400번대 에러가 반환되면 '클라이언트 측 오류'가 콘솔에 출력된다.", () => {})
+
+  // it("400번 미만의 에러가 반환되면 '데이터 fetch 에류'가 콘솔에 출력된다.", () => {})
 })
