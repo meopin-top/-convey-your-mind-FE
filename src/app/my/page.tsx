d2f2c567de22dd0b2d1a8ea7a77fe651b6a2209b
mochang2 <qjsrodksro@gmail.com>
2023-11-20 14:34:26 +0900

    feat: 마이페이지 참여 중인 프로젝트 및 내가 받은 롤링페이퍼 API 연동, 서버 컴포넌트 마이 페이지 최상단으로 이동

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -1,14 +1,102 @@
-import {NeedLoggedIn, Header} from "@/components"
+import {Suspense} from "react"
+import Link from "next/link"
+import {calculateRemainingDay} from "@/utils/formatter"
+import {PROJECT_TYPE} from "@/constants/request"
+import {NeedLoggedIn, Header, Loading} from "@/components"
 import {
   UserInformation,
   RollingPaperParticipation,
-  ProjectsInProgressing,
-  ReceivedRollingPapers,
+  AllProjects,
+  AllReceivedRollingPapers,
 } from "@/components/my"
 
-const MyPage = () => {
+// TODO: API 명세 작성되면 각각 알파벳 매핑 변경
+const statusMapper = {
+  D: "참여 완료",
+  E: "작성 전",
+  F: "전달 완료",
+} as const
+
+type TResponse = {
+  id: number
+  title: string
+  description: string
+  inviteCode: string
+  maxInviteNum: number
+  destination: string
+  type: (typeof PROJECT_TYPE)[keyof typeof PROJECT_TYPE]
+  status: keyof typeof statusMapper
+  expiredDatetime: string
+  owner: boolean
+}
+
+// const projectsMockData: TResponse[] = [
+//   {
+//     id: 1,
+//     title: "관리자 프로젝트 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ",
+//     description: "관리자 프로젝트 설명",
+//     inviteCode: "관리자코드",
+//     maxInviteNum: 10,
+//     destination: "관리자",
+//     type: "D",
+//     status: "D",
+//     expiredDatetime: "2023-09-30T04:55:55.272243",
+//     owner: true,
+//   },
+//   {
+//     id: 2,
+//     title: "관리자 프로젝트",
+//     description: "관리자 프로젝트 설명",
+//     inviteCode: "관리자코드",
+//     maxInviteNum: 10,
+//     destination: "관리자",
+//     type: "D",
+//     status: "E",
+//     expiredDatetime: "2023-09-30T04:55:55.272243",
+//     owner: false,
+//   },
+//   {
+//     id: 3,
+//     title: "관리자 프로젝트",
+//     description: "관리자 프로젝트 설명",
+//     inviteCode: "관리자코드",
+//     maxInviteNum: 10,
+//     destination: "관리자",
+//     type: "D",
+//     status: "F",
+//     expiredDatetime: "2023-09-30T04:55:55.272243",
+//     owner: false,
+//   },
+// ]
+
+const MyPage = async () => {
+  const fetchPromises = [
+    fetch(
+      `${process.env.NEXT_PUBLIC_API_HOST}/api/projects/page?pageSize=3&pageNum=1&type=${PROJECT_TYPE.PARTICIPANT}`
+    ),
+    fetch(
+      `${process.env.NEXT_PUBLIC_API_HOST}/api/projects/page?pageSize=4&pageNum=1&type=${PROJECT_TYPE.RECEIVER}`
+    ),
+  ]
+
+  const jsonPromises: Promise<{data: {pageResult: TResponse[]}}>[] =
+    await Promise.all(fetchPromises).then(
+      ([fetchedProjects, fetchedRollingPapers]) => [
+        fetchedProjects.json(),
+        fetchedRollingPapers.json(),
+      ]
+    )
+  let [
+    {
+      data: {pageResult: projects},
+    },
+    {
+      data: {pageResult: rollingPapers},
+    },
+  ] = await Promise.all(jsonPromises)
+
   return (
-    <>
+    <Suspense fallback={<Loading isLoading />}>
       <NeedLoggedIn />
 
       <div className="my-page root-wrapper">
@@ -17,11 +105,75 @@ const MyPage = () => {
         <main className="main f-center">
           <UserInformation />
           <RollingPaperParticipation />
-          <ProjectsInProgressing />
-          <ReceivedRollingPapers />
+          <div className="projects">
+            <div className="header mb-2">
+              <h5 className="title"># 참여 중인 프로젝트</h5>
+              <AllProjects />
+            </div>
+
+            {projects.length > 0 ? (
+              <ul>
+                {projects.map((project) => (
+                  <li key={project.id} className="project mb-2">
+                    {project.owner && <span className="crown">👑</span>}
+                    <span className="name txt-ellipsis">{project.title}</span>
+                    {project.status !== "D" && (
+                      <span className="until">
+                        D-
+                        {Math.max(
+                          Math.min(
+                            calculateRemainingDay(project.expiredDatetime),
+                            999
+                          ),
+                          0
+                        )}
+                      </span>
+                    )}
+                    <span
+                      className={`status ${project.status} f-center radius-xl mr-1 ml-1`}
+                    >
+                      {statusMapper[project.status]}
+                    </span>
+                    <span className="to">
+                      <Link
+                        href={`rolling-paper/edit/${project.inviteCode}`}
+                      >{`> 바로 가기`}</Link>
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <div className="no-project">참여 중인 프로젝트가 없습니다.</div>
+            )}
+          </div>
+          <div className="rolling-papers">
+            <div className="header mb-2">
+              <h5 className="title"># 내가 받은 롤링페이퍼 💌</h5>
+              <AllReceivedRollingPapers />
+            </div>
+
+            {rollingPapers.length > 0 ? (
+              <ul className="rolling-paper">
+                {rollingPapers.map((rollingPaper) => (
+                  <li key={rollingPaper.id} className="shadow-sm">
+                    <Link
+                      href={`rolling-paper/view/${rollingPaper.inviteCode}`}
+                      className="f-center pl-2 pr-2"
+                    >
+                      <div className="name">{rollingPaper.title}</div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <div className="no-rolling-paper">
+                받은 롤링페이퍼가 없습니다.
+              </div>
+            )}
+          </div>
         </main>
       </div>
-    </>
+    </Suspense>
   )
 }
 
