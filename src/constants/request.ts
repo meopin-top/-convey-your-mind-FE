2c25237d73f2c61a5f2a5c78ec8d2d5f4c2bbea9
mochang2 <qjsrodksro@gmail.com>
2023-11-26 15:21:43 +0900

    refactor: 프로젝트 정보 상수, 타입 공통으로 분리

--- src/constants/request.ts
+++ src/constants/request.ts
@@ -1,8 +1,14 @@
 export const ROLLING_PAPER_TYPE = {
   FREELY: "D", // default
-}
+} as const
 
 export const PROJECT_TYPE = {
   PARTICIPANT: "E",
   RECEIVER: "D",
-}
+} as const
+
+export const ROLLING_PAPER_STATUS = {
+  R: "작성 전", // READY
+  C: "참여 완료", // COMPLETE
+  D: "전달 완료", // DELIVERED
+} as const
