dd353bc5abcf21a36df57f006c97e4a924be8703
mochang2 <mochang2@gmail.com>
2024-02-10 16:56:21 +0900

    feat: 뷰포트가 변경되어도 화면 내 일정한 크기가 유지되는 기능 구현

---
+++ src/hooks/use-size-keeper.ts
@@ -0,0 +1,65 @@
+"use client"
+
+import {useState, useEffect} from "react"
+import {debounce} from "@/utils/optimization"
+
+export default function useSizeKeeper(initialSizes: number[]) {
+  const [sizes, setSizes] = useState([...initialSizes])
+  const [position, setPosition] = useState<{
+    top: number
+    left: number
+    width: number
+  }>({
+    top: 0,
+    left: 0,
+    width: 0,
+  })
+
+  useEffect(() => {
+    if (!visualViewport || visualViewport.height === 0) {
+      return
+    }
+
+    const initialHeight = visualViewport.height
+
+    function maintainSize() {
+      if (!visualViewport || visualViewport.height === 0) {
+        return
+      }
+
+      const currentHeight = visualViewport.height
+      const ratio = currentHeight / initialHeight
+
+      setSizes(initialSizes.map((size) => size * ratio))
+      setPosition({
+        top: visualViewport.offsetTop,
+        left: visualViewport.offsetLeft,
+        width: visualViewport.width,
+      })
+    }
+
+    const maintainSizeWithDelay = debounce(maintainSize, 50)
+
+    function pinchZoomResizeHandler(event: TouchEvent) {
+      if (event.touches.length !== 2) {
+        return
+      }
+
+      maintainSizeWithDelay()
+    }
+
+    window.addEventListener("resize", maintainSize)
+    window.addEventListener("touchmove", pinchZoomResizeHandler)
+    window.addEventListener("wheel", maintainSize)
+
+    return () => {
+      window.removeEventListener("resize", maintainSize)
+      window.removeEventListener("touchmove", pinchZoomResizeHandler)
+      window.removeEventListener("wheel", maintainSize)
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return {sizes, position}
+}
