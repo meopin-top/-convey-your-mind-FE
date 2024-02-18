389cc7d23280d80c95e9c2a7d7007ac899d99a6f
mochang2 <mochang2@gmail.com>
2024-02-18 23:30:13 +0900

    refactor: useInput이 textarea도 포함할 수 있도록 타입 수정

--- src/hooks/use-input.ts
+++ src/hooks/use-input.ts
@@ -2,13 +2,19 @@ import {useState, type ChangeEvent} from "react"
 
 export type TInputChangeEvent = ChangeEvent<HTMLInputElement>
 
+export type TTextareaChangeEvent = ChangeEvent<HTMLTextAreaElement>
+
 export default function useInput(
   defaultValue: string = "",
-  callback?: (event: TInputChangeEvent) => any
-): [string, (event: TInputChangeEvent) => void, (value: string) => void] {
+  callback?: (event: TInputChangeEvent | TTextareaChangeEvent) => any
+): [
+  string,
+  (event: TInputChangeEvent | TTextareaChangeEvent) => void,
+  (value: string) => void
+] {
   const [value, setValue] = useState(defaultValue)
 
-  function handleValue(event: TInputChangeEvent) {
+  function handleValue(event: TInputChangeEvent | TTextareaChangeEvent) {
     setValue(event.target.value)
     if (callback) {
       callback(event)
